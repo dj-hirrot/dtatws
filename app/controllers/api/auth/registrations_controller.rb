e14284b0f93b13627f01d2b29541a3bb3851db36
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-29 23:17:25 +0900

    customize

---
+++ app/controllers/api/auth/registrations_controller.rb
@@ -0,0 +1,10 @@
+class Api::Auth::RegistrationsController < ApplicationController
+  private
+    def sign_up_parmas
+      params.permit(:name, :email, :company, :password, :password_confirmation)
+    end
+
+    def account_update_params
+      params.permit(:name, :email, :company)
+    end
+end
